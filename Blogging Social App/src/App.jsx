54927accb3cffbeaa756f2c6188d0143449b3585
woodyshekinah <woody.shekinah@gmail.com>
2024-05-24 03:10:30 -0700

    Added the required images to the public folder

--- Blogging Social App/src/App.jsx
+++ Blogging Social App/src/App.jsx
@@ -1,12 +1,14 @@
-import { Button} from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './Pages/HomePage/HomePage'
 
 function App() {
 
   return (
     <>
-      <Button>
-        Hello World!
-      </Button>
+    { /* //coming from react router dom */ }
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+      </Routes>
     </>
   )
 }
